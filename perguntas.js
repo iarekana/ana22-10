9490e41179546969894456b6c9a86dcde05a9e61
iarekana <137410298+iarekana@users.noreply.github.com>
2024-10-22 09:51:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'musica',
+    'O que é musica?',
+    'é a combinação de ritmo '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é madrid'
+)
+
+criaCartao(
+    'trabalho',
+    'O que é um trabalho',
+    'cunjunto de atividades'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em Inglês?',
+    'eu te amo em ingles é ilove you'
+)
